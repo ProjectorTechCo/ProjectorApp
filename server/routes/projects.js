6cd7205692ac895fb165af1a0d05140eb83bad89
Ilan Kosteniov <ikosteniov97@gmail.com>
2020-03-04 23:14:21 +0200

    able to fetch from projects

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,9 +2,14 @@ const express = require("express");
 const PgInstance = require("./../PgInstance");
 
 const router = express.Router();
+const PROJECTS = "comp_projects";
 
-router.get('/', async (req, res) => {
-    let {status, message} = await PgInstance.action("SELECT * FROM comp_projects");
+router.get('/:proj_id?', async (req, res) => {
+    let { proj_id } = req.params;
+    let query = `SELECT * FROM ${PROJECTS}`;
+    if (proj_id)
+        query += ` WHERE proj_id = ${proj_id}`;
+    let {status, message} = await PgInstance.action(query);
     res.status(status).json(message);
 });
 
